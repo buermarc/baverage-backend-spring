f4dafffe1359df00868ece43d043b2bfab9b0cab
mulc <buermarc2@gmail.com>
2021-06-20 23:57:22 +0200

    feat(NewBestellung): Add dto class for new bestellung

---
+++ src/main/java/com/baverage/backend/dto/NewBestellung.java
@@ -0,0 +1,20 @@
+package com.baverage.backend.dto;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NewBestellung implements Serializable {
+    int platz_id;
+    int getraenk_id;
+    int kunde_id;
+}
