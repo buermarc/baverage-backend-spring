3f55df27a41b0ecc81c1f891951ea0fe5ff48a7d
buermarc <buermarc2@gmail.com>
2021-06-25 09:29:44 +0200

    feat(Main Logic): Add Mqtt handler

--- src/main/java/com/baverage/backend/BackendApplication.java
+++ src/main/java/com/baverage/backend/BackendApplication.java
@@ -5,15 +5,28 @@ import java.sql.SQLException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.baverage.backend.DatabaseConnection.DBConnection;
+import com.baverage.backend.MqttPaho.Mqtt;
 
 @SpringBootApplication
 public class BackendApplication {
 
+        private static final Logger LOGGER = LoggerFactory.getLogger(BackendApplication.class);
+
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		SpringApplication.run(BackendApplication.class, args);
+                /*
 		DBConnection db = new DBConnection();
 		db.connectToDatabase();
+                */
+                try {
+                    Mqtt mqtt = new Mqtt();
+                } catch (Exception e) {
+                    LOGGER.error(e.toString());
+                }
 
 	}
 
