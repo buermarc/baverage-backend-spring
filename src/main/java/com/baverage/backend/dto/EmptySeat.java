9991c62de8fab249483b5e3b52ef6de7b74a0f8f
mulc <buermarc2@gmail.com>
2021-06-25 10:15:26 +0200

    feat(EmptySeat): Add EmptySeat dto class

---
+++ src/main/java/com/baverage/backend/dto/EmptySeat.java
@@ -0,0 +1,20 @@
+package com.baverage.backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmptySeat {
+    private int id;
+    private int tisch_id;
+    private int platz_id;
+    private int plaetze_am_tisch;
+    private double fuellstand;
+}
