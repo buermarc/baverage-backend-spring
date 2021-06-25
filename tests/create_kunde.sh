c8e62a792275fba6f49f71b7b04a2ad58f7b5d45
buermarc <buermarc2@gmail.com>
2021-06-25 09:30:18 +0200

    chore(tests): Change create-kunde test to contain platz id

--- tests/create_kunde.sh
+++ tests/create_kunde.sh
@@ -1,4 +1,4 @@
 curl --header "Content-Type: application/json" \
   --request POST \
-  --data '{ "name": "A new user with a very special name"} ' \
+  --data '{ "name": "A new user with a very special name", "platz_id": 1} ' \
   http://localhost:8080/api/createKunde
