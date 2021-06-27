916be6a94b4aebca92f0fbb6aaf0f67fe0f164be
mulc <buermarc2@gmail.com>
2021-06-27 22:03:08 +0200

    feat(MesspunktRepo): Addition of Repo for Messpunkte

---
+++ src/main/java/com/baverage/backend/repo/MesspunktRepo.java
@@ -0,0 +1,13 @@
+package com.baverage.backend.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.baverage.backend.DatabaseConnection.Messpunkte;
+
+/**
+ * Erlaubt angepasste Export Mappings des Repositories
+ */
+@org.springframework.stereotype.Repository
+public interface MesspunktRepo extends CrudRepository<Messpunkte, Integer>{
+	
+}
