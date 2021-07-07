da19a62c097679fed2851eaf12fc4d441f8f51d4
Nico Rubach <nico.rubach@web.de>
2021-07-07 16:30:36 +0200

    Updated Timer

--- src/main/resources/static/js/timer.js
+++ src/main/resources/static/js/timer.js
@@ -47,20 +47,35 @@ startOffsetTimer = (span, order) => {
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
     let t = order.zeitpunkt_vorbereitet.split(/[T : .]/); //Zeitstempel in Bestellung per RegEx an "T, : und ." spalten
+    //t[0] -> Datum
+    //t[1] -> Stunde
+    //t[2] -> Minute
+    //t[3] -> Sekunde
     let minutesSinceStart;
     let secondsSinceStart;
-    t[1] += 2; //Zeitzonenanpassung
-
+    t[1] = parseInt(t[1]) + 2; //Zeitzonenanpassung
     if(hour > t[1]) { //Wenn die aktuelle Stunde nach der Bestellstunde liegt
         minutesSinceStart = 60 - t[2] + minutes; //Rest zur vollen Stunde + aktuelle Minuten
+    } else if(hour < t[1]) {
+        minutesSinceStart = 60 - t[2] + minutes;
     } else if(hour == t[1]) {
         minutesSinceStart = minutes - t[2]; //Aktuelle Minute - Bestellminute
     }
 
     if(minutes > t[2]) { //Wenn die aktuelle Minute nach der Bestellminute liegt
-        secondsSinceStart = 60 - t[3] + seconds; //Restliche Sekunden zur vollen Minute + aktuelle Sekunden
-    } else {
+        if(seconds < t[3]) {
+            minutesSinceStart -= 1;
+            secondsSinceStart = 60 - t[3] + seconds; //Restliche Sekunden zur vollen Minute + aktuelle Sekunden
+        } else if(seconds > t[3]) {
+            secondsSinceStart = seconds - t[3]; //Aktuelle Sekunde - Startsekunde
+        } else if(seonds == t[3]) {
+            secondsSinceStart = 0;
+        }        
+    } else if(minutes < t[2]) {
+        secondsSinceStart = 60 - t[3] + seconds;
+    } else if (minutes == t[2]) {
         secondsSinceStart = seconds - t[3]; //Aktuelle Sekunde - Startsekunde
     }
+    console.log(minutesSinceStart + " : " + secondsSinceStart);
     startTimer(span, minutesSinceStart, secondsSinceStart);
 }
