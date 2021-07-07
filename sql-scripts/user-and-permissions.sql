56779399e9b81a3ebef4e950b458ce645c09bd28
mulc <buermarc2@gmail.com>
2021-07-07 15:13:35 +0200

    feat(sql-scripts): Create user and permissions based on env var

--- sql-scripts/user-and-permissions.sql
+++ sql-scripts/user-and-permissions.sql
@@ -1,3 +1,3 @@
-create user 'iot'@'10.0.2.100' identified by '1234';
-grant all privileges on app.* to 'iot'@'10.0.2.100';
+create user 'MYSQL_U'@'10.0.2.100' identified by 'MYSQL_P';
+grant all privileges on MYSQL_DB.* to 'MYSQL_P'@'10.0.2.100';
 flush privileges;
