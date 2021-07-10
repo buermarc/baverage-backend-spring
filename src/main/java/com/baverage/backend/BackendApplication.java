b9ceae008b5b9638a78790483f1193c431ea7b05
mulc <buermarc2@gmail.com>
2021-07-10 15:38:44 +0200

    chore(BackendApplication): Cleanup
    Remove unnecessary imports and member variables

--- src/main/java/com/baverage/backend/BackendApplication.java
+++ src/main/java/com/baverage/backend/BackendApplication.java
@@ -4,17 +4,9 @@ import java.sql.SQLException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.event.Level;
-
-import com.baverage.backend.databaseConnection.DBConnection;
-import com.baverage.backend.mqttPaho.Mqtt;
 
 @SpringBootApplication
 public class BackendApplication {
-
-        private static final Logger LOGGER = LoggerFactory.getLogger(BackendApplication.class);
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		SpringApplication.run(BackendApplication.class, args);
